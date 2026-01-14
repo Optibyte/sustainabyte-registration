46a879c583dbf8dea0238c537159e2cd2434b646
Optibyte <optibyte@sustainabyte.ai>
2026-01-14 15:18:53 +0530

    add

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // output: "export",
-  basePath: '/facility',
-  assetPrefix: '/facility/',
 };
 
 export default nextConfig;  
