cef3032d9589e82be966f5295693faae0a9dd688
Optibyte <optibyte@sustainabyte.ai>
2026-01-21 16:17:44 +0530

    ad

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,14 @@ const nextConfig = {
   devIndicators: {
     buildActivity: false,
   },
- 
+  allowedDevOrigins: ["sustainabyte.ai"],
+  onDemandEntries: {
+    maxInactiveAge: 60 * 1000,
+    pagesBufferLength: 5,
+  },
+  experimental: {
+    optimizePackageImports: ["@radix-ui"],
+  },
 };
 
 export default nextConfig;
